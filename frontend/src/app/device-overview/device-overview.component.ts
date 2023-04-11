48c0262df4b817871c3555e33ee3879a516a9487
Ståle Dahl <stalehd@gmail.com>
2023-04-11 11:10:09 +0200

    Remove all chart

--- frontend/src/app/device-overview/device-overview.component.ts
+++ frontend/src/app/device-overview/device-overview.component.ts
@@ -24,9 +24,6 @@ export class DeviceOverviewComponent implements OnInit, AfterViewInit, OnDestroy
     @ViewChild("legend") legendRef?: ElementRef;
     legend?: (SVGElement | HTMLElement);
 
-    @ViewChild("allchart") allChartRef?: ElementRef;
-    allChart?: (SVGElement | HTMLElement);
-
     data: DeviceSample[] = [];
     constructor(
         protected samples: SampleService,
@@ -54,10 +51,6 @@ export class DeviceOverviewComponent implements OnInit, AfterViewInit, OnDestroy
             // Remove the old one if it already exists
             this.renderer.removeChild(this.chartRef?.nativeElement, this.chart, false);
         }
-        if (this.allChart) {
-            // Remove the old one if it already exists
-            this.renderer.removeChild(this.allChartRef?.nativeElement, this.allChart, false);
-        }
         if (this.legend) {
             this.renderer.removeChild(this.legendRef?.nativeElement, this.legend, false);
         }
@@ -66,10 +59,6 @@ export class DeviceOverviewComponent implements OnInit, AfterViewInit, OnDestroy
         if (this.chart) {
             // Remove the old one if it already exists
             this.renderer.removeChild(this.chartRef?.nativeElement, this.chart, false);
-        }
-        if (this.allChart) {
-            // Remove the old one if it already exists
-            this.renderer.removeChild(this.allChartRef?.nativeElement, this.allChart, false);
         }
 
         let width = this.chartRef?.nativeElement.offsetWidth;
@@ -130,85 +119,5 @@ export class DeviceOverviewComponent implements OnInit, AfterViewInit, OnDestroy
             },)
             this.renderer.appendChild(this.legendRef?.nativeElement, this.legend)
         }
-
-        // Add chart for ALL THE THINGS. Rearrange the samples to make charting easier. Add a separate sample for 
-        // each value.
-
-        const mmFormat = d3.timeFormat("%d %H:00")
-
-        let sortedSamples = this.getFlattenedSamples();
-        let startDate = d3.min(this.data, (d: DeviceSample) => d.time);
-        let endDate = d3.max(this.data, (d: DeviceSample) => d.time)
-
-        let allWidth = this.allChartRef?.nativeElement.offsetWidth;
-        this.allChart = Plot.plot({
-            width: allWidth,
-            margin: 50,
-            inset: 5,
-            y: {
-                grid: true,
-                label: "Antall"
-            },
-            x: {
-                label: "Klokkeslett",
-                grid: true,
-                tickFormat: d3.timeFormat("%H:%M"),
-                domain: [startDate, endDate]
-            },
-            marks: [
-                Plot.line(
-                    sortedSamples,
-                    Plot.windowY(
-                        { reduce: "sum", k: 180, anchor: "middle" },
-                        {
-                            x: "time",
-                            y: "count",
-                            stroke: "type",
-                            strokeWidth: 2,
-                            opacity: 0.8,
-                        }
-                    )
-                ),
-                Plot.frame(),
-            ],
-            color: {
-                legend: false,
-                domain: ["wifi", "ble"],
-                range: ["red", "blue"]
-            },
-            style: {
-                fontFamily: 'sans-serif',
-                fontSize: '10pt',
-                background: '#eeeeee',
-                fill: '#808080',
-            }
-        });
-        this.renderer.appendChild(this.allChartRef?.nativeElement, this.allChart)
-    }
-
-    // Return a sorted list with flat samples for all the data
-    getFlattenedSamples(): FlatSample[] {
-        let ret: FlatSample[] = this.data.map((d: DeviceSample) => {
-            let r: FlatSample = {
-                id: d.id,
-                name: d.name,
-                time: d.time,
-                count: d.ble,
-                type: "ble",
-            };
-            return r;
-        }).concat(
-            this.data.map((d: DeviceSample) => {
-                let r: FlatSample = {
-                    id: d.id,
-                    name: d.name,
-                    time: d.time,
-                    count: d.wifi,
-                    type: "wifi",
-                };
-                return r;
-            }
-            ));
-        return d3.sort(ret, (a: FlatSample, b: FlatSample) => a.time.getTime() - b.time.getTime());
     }
 }
